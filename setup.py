5c1c96ae5401da3da8ce1810880009fd4aa3779b
Rajas Sambhare <sambhare@users.noreply.github.com>
2022-06-10 17:02:39 -0400

    Restrict protobuf to <4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='dfx-apiv2-protos',
     version=__version__,
     packages=['dfx_apiv2_protos'],
-    install_requires=['protobuf>=3.12'],
+    install_requires=['protobuf>=3.12,<4'],
     setup_requires=['wheel'],
     description='dfx-apiv2-protos are compiled Python protos for the Nuralogix DeepAffex API.',
 )
