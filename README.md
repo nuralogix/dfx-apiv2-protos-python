c0413b68a768428254e28b532e2249e7de5524de
Rajas Sambhare <sambhare@users.noreply.github.com>
2022-09-13 09:58:58 -0400

    Updated setup.py with author, classifiers, etc.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# DFX APIv2 Protocol Buffers
+# DeepAffex APIv2 Protocol Buffers
 
-This repository contains pre-compiled Python 3 protos for the Nuralogix DeepAffex
-API.
+This repository contains pre-compiled Python 3 Protocol Buffers for the
+NuraLogix DeepAffex API.
 
 For more information, please visit https://deepaffex.ai/developers-api
--- setup.py
+++ setup.py
@@ -4,9 +4,17 @@ from dfx_apiv2_protos import __version__
 
 setup(
     name='dfx-apiv2-protos',
+    author="NuraLogix Corporation",
     version=__version__,
     packages=['dfx_apiv2_protos'],
     install_requires=['protobuf>=3.12,<4'],
     setup_requires=['wheel'],
     description='dfx-apiv2-protos are compiled Python protos for the Nuralogix DeepAffex API.',
+    readme="README.md",
+    license_file='LICENSE.txt',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
