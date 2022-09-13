edb5a785af12b68169bf156b3613307ef981e91f
Rajas Sambhare <sambhare@users.noreply.github.com>
2022-09-13 19:35:09 -0400

    Added long_description from README

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
+import pathlib
+
 from setuptools import setup
 
 from dfx_apiv2_protos import __version__
+
+long_description = (pathlib.Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='dfx-apiv2-protos',
@@ -10,6 +14,8 @@ setup(
     install_requires=['protobuf>=3.12,<4'],
     setup_requires=['wheel'],
     description='dfx-apiv2-protos are compiled Python protos for the Nuralogix DeepAffex API.',
+    long_description=long_description,
+    long_description_content_type='text/x-markdown',
     license_file='LICENSE.txt',
     classifiers=[
         "Programming Language :: Python :: 3",
